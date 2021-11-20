8af971de0ccae01393f3b61954f6bfc74448c966
LuongThiQuynhHuong <holmestk24@gmail.com>
2021-11-20 11:16:33 +0700

    renew menu.php

--- POS_PHP/src/menu.php
+++ POS_PHP/src/menu.php
@@ -23,6 +23,8 @@ else{
     if($order["status"] == 1){
         $sql = "INSERT INTO `order`(`status`) VALUES(0);";
         $order = mysqli_query($conn, $sql);
+        $sql = "SELECT * FROM `order` ORDER BY `ID` DESC LIMIT 1;";
+        $order = mysqli_query($conn, $sql);
         $order = mysqli_fetch_assoc($order);
     }
 }
@@ -37,7 +39,6 @@ $category = mysqli_query($conn, $sql);
 
 $sql = "SELECT * FROM `product`;";
 $product = mysqli_query($conn, $sql);
-$modal = mysqli_query($conn, $sql);
 
 $sql = "SELECT * FROM `table`;";
 $table = mysqli_query($conn, $sql);
@@ -114,8 +115,10 @@ mysqli_close($conn);
                         </div>
     
                         <div class="col-sm-6" id="search-container" style="background-color:#fff;">
-                            <input style="margin-top: 15px; margin-bottom:10px; margin-left:15%;" type="search" name="search-box" id="search-box">
-                            <button ><img src="../images/search.png" height="17px"></button>
+                        <form action="search.php" class="d-flex searchTool" method="GET">
+                            <input style="margin-top: 15px; margin-bottom:10px; margin-left:15%;" type="search" name="search" id="search">
+                            <button type="submit" style="height:30px;margin-bottom:10px;margin-top:15px;"><img src="../images/search.png" height="17px"></button>
+                        </form>
                         </div>
                     </div>
 
@@ -164,6 +167,85 @@ mysqli_close($conn);
                             </div>
                         </div>
                     </div>
+
+                    <div class="modal fade" id="Modal<?php echo $row['ID']; ?>" tabindex="-1" aria-labelledby="ItemModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+      
+            <!-- Modal Header -->
+                <div class="modal-header" style="background-color: #F0F0F0;">          
+                    <b style="color:#2C3A57; font-size:22px; padding-left:10px;">Thông tin món ăn</b>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+      
+            <!-- Modal body -->
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-3" style=" padding-top: 10px;">
+                            <div style="border:solid; border-width:3px; height:200px; width:170px;padding:7px;border-color:#F0F0F0;border-radius:10%">
+                                <img src="<?php echo $row['IMG']; ?>" height="155px">
+                            </div>
+                        </div>
+                        <div class="col-md-9" >
+                            <div class="row">
+                                <div style="width:20%;">
+                                    <b style="color:#2C3A57; font-size:18px; padding-left:10px;">ID</b><br>
+                                    <b style="color:#2C3A57; font-size:15px; padding-left:10px;"><?php echo $row['ID']; ?></b>
+                                </div>
+                                <div style="width:40%;">
+                                    <b style="color:#2C3A57; font-size:18px; padding-left:10px;">Tên món </b><br>
+                                    <b style="color:#2C3A57; font-size:15px; padding-left:10px;"><?php echo $row['name']; ?></b>
+                                </div>
+                                <div style="width:40%; text-align:right;">
+                                    <b style="color:#2C3A57; font-size:18px; padding-left:10px;">Đơn giá </b><br>
+                                    <b style="color: red; font-size:20px;padding-left:10px;"><?php echo $row['price']; ?> đ</b>
+                                </div>
+                            </div>
+                            <hr style="margin-top: 20px; width: 99%; color:#C4C4C4;">
+                            <div class="row">
+                                <div style="width:50%;">
+                                    <b style="color:#2C3A57; font-size:20px; padding-left:10px;">Số lượng </b>
+                                </div>
+                                <div style="width:50%; text-align:right;">
+                                    <button onclick="javascript:addOrMinusBtnInModal('amount<?php echo $row['ID']; ?>', 'totalprice<?php echo $row['ID']; ?>', <?php echo $row['price']; ?>, 0)" style="width: 30px; height:30px"><b>-</b></button>                               
+                                    <b id="amount<?php echo $row['ID']; ?>" style="color:#2C3A57; font-size:18px; padding-left:5px;">
+                                        1
+                                    </b>
+                                    <button onclick="javascript:addOrMinusBtnInModal('amount<?php echo $row['ID']; ?>', 'totalprice<?php echo $row['ID']; ?>', <?php echo $row['price']; ?>, 1)" style="width: 30px; height:30px"><b style="color:red;">+</b></button>
+                                </div>
+                            </div>
+                            <hr style="margin-top: 20px; width: 99%; color:#C4C4C4;">
+                            <p style="color:#2C3A57; font-size:16px; padding-left:5px;"><b>Thông tin dinh dưỡng: </b>
+                            <?php echo $row['nutrient']; ?></p>
+                            <p style="color:#2C3A57; font-size:16px; padding-left:5px;"><b>Phụ gia: </b>
+                            <?php echo $row['additives']; ?></p>
+                            <p style="color:#2C3A57; font-size:16px; padding-left:5px;"><b>Trang trí món ăn: </b>
+                            <?php echo $row['decoration']; ?></p>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <b style="color:#2C3A57;font-size:12pt;padding-left:5px;">Yêu cầu đặc biệt: </b>
+                                    <p style="color:#2C3A57;font-size:10pt;padding-left:5px;">(Không bắt buộc)</p>
+                                </div>
+                                <div class="col-sm-8">
+                                    <textarea id="note<?php echo $row['ID']; ?>" class="form-control" aria-label="With textarea" placeholder="VD: không bỏ hành"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+      
+            <!-- Modal footer -->
+            <div class="modal-footer" style="text-align: right;">
+              <button onclick = "javascript:UpdateOrAddFromModal(<?php echo $row['ID']?>,'note<?php echo $row['ID']; ?>','totalprice<?php echo $row['ID']; ?>','amount<?php echo $row['ID']; ?>')" style="width:72%; background-color:red; color:white" type="button"  data-bs-dismiss="modal">
+                  <img src="../images/cart1.png" height="30px">
+                  <b id="totalprice<?php echo $row['ID']; ?>"><?php echo $row['price']; ?></b><b> đ</b>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
                 <?php }} ?>
                 </div>
             </div>
@@ -245,86 +327,6 @@ mysqli_close($conn);
             </div>
         </div>
     </div>
-
-
-    <?php if (mysqli_num_rows($modal) > 0) {while ($row = mysqli_fetch_assoc($modal)) { ?>
-    <div class="modal fade" id="Modal<?php echo $row['ID']; ?>" tabindex="-1" aria-labelledby="ItemModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-      
-            <!-- Modal Header -->
-                <div class="modal-header" style="background-color: #F0F0F0;">          
-                    <b style="color:#2C3A57; font-size:22px; padding-left:10px;">Thông tin món ăn</b>
-                    <button onclick = "location.reload()" type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                </div>
-      
-            <!-- Modal body -->
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="col-md-3" style=" padding-top: 10px;">
-                            <div style="border:solid; border-width:3px; height:200px; width:170px;padding:7px;border-color:#F0F0F0;border-radius:10%">
-                                <img src="<?php echo $row['IMG']; ?>" height="155px">
-                            </div>
-                        </div>
-                        <div class="col-md-9" >
-                            <div class="row">
-                                <div style="width:20%;">
-                                    <b style="color:#2C3A57; font-size:18px; padding-left:10px;">ID</b><br>
-                                    <b style="color:#2C3A57; font-size:15px; padding-left:10px;"><?php echo $row['ID']; ?></b>
-                                </div>
-                                <div style="width:40%;">
-                                    <b style="color:#2C3A57; font-size:18px; padding-left:10px;">Tên món </b><br>
-                                    <b style="color:#2C3A57; font-size:15px; padding-left:10px;"><?php echo $row['name']; ?></b>
-                                </div>
-                                <div style="width:40%; text-align:right;">
-                                    <b style="color:#2C3A57; font-size:18px; padding-left:10px;">Đơn giá </b><br>
-                                    <b style="color: red; font-size:20px;padding-left:10px;"><?php echo $row['price']; ?> đ</b>
-                                </div>
-                            </div>
-                            <hr style="margin-top: 20px; width: 99%; color:#C4C4C4;">
-                            <div class="row">
-                                <div style="width:50%;">
-                                    <b style="color:#2C3A57; font-size:20px; padding-left:10px;">Số lượng </b>
-                                </div>
-                                <div style="width:50%; text-align:right;">
-                                    <button onclick="javascript:addOrMinusBtnInModal('amount<?php echo $row['ID']; ?>', 'totalprice<?php echo $row['ID']; ?>', <?php echo $row['price']; ?>, 0)" style="width: 30px; height:30px"><b>-</b></button>                               
-                                    <b id="amount<?php echo $row['ID']; ?>" style="color:#2C3A57; font-size:18px; padding-left:5px;">
-                                        1
-                                    </b>
-                                    <button onclick="javascript:addOrMinusBtnInModal('amount<?php echo $row['ID']; ?>', 'totalprice<?php echo $row['ID']; ?>', <?php echo $row['price']; ?>, 1)" style="width: 30px; height:30px"><b style="color:red;">+</b></button>
-                                </div>
-                            </div>
-                            <hr style="margin-top: 20px; width: 99%; color:#C4C4C4;">
-                            <p style="color:#2C3A57; font-size:16px; padding-left:5px;"><b>Thông tin dinh dưỡng: </b>
-                            <?php echo $row['nutrient']; ?></p>
-                            <p style="color:#2C3A57; font-size:16px; padding-left:5px;"><b>Phụ gia: </b>
-                            <?php echo $row['additives']; ?></p>
-                            <p style="color:#2C3A57; font-size:16px; padding-left:5px;"><b>Trang trí món ăn: </b>
-                            <?php echo $row['decoration']; ?></p>
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <b style="color:#2C3A57;font-size:12pt;padding-left:5px;">Yêu cầu đặc biệt: </b>
-                                    <p style="color:#2C3A57;font-size:10pt;padding-left:5px;">(Không bắt buộc)</p>
-                                </div>
-                                <div class="col-sm-8">
-                                    <textarea id="note<?php echo $row['ID']; ?>" class="form-control" aria-label="With textarea" placeholder="VD: không bỏ hành"></textarea>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-      
-            <!-- Modal footer -->
-            <div class="modal-footer" style="text-align: right;">
-              <button onclick = "javascript:UpdateOrAddFromModal(<?php echo $row['ID']?>,'note<?php echo $row['ID']; ?>','totalprice<?php echo $row['ID']; ?>','amount<?php echo $row['ID']; ?>')" style="width:72%; background-color:red; color:white" type="button"  data-bs-dismiss="modal">
-                  <img src="../images/cart1.png" height="30px">
-                  <b id="totalprice<?php echo $row['ID']; ?>"><?php echo $row['price']; ?></b><b> đ</b>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    <?php }} ?>
 
     <script type="text/javascript">
         // add: mode = 1; minus: mode = 0
